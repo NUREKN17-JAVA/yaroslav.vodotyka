ba744b6a1dcf86958d05a9d8a780cfab4f13a51b
Yaroslav Vodotyka <yaroslav.vodotyka@nure.ua>
2019-11-17 12:32:38 +0200

    HsqlDao and HsqlDao test class created

---
+++ src/main/java/db/HsqlDbUserDao.java
@@ -0,0 +1,38 @@
+package db;
+
+import java.util.Collection;
+
+import nure.cs.vodotyka.usermanagment.User;
+
+public class HsqlDbUserDao implements UserDao {
+
+	public HsqlDbUserDao() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public User CreateUser(User user) throws DatabaseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void UpdateUser(long userId) throws DatabaseException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void DeleteUser(long userId) throws DatabaseException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public User GetUser(long userId) throws DatabaseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Collection GetAll() throws DatabaseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/test/java/db/HsqlDbUserDaoTest.java
@@ -0,0 +1,19 @@
+package db;
+
+import junit.framework.TestCase;
+
+public class HsqlDbUserDaoTest extends TestCase {
+
+	HsqlDbUserDao dao;
+	
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		dao = new HsqlDbUserDao();
+	}
+
+	public void testCreateUser() {
+		
+	}
+
+}
