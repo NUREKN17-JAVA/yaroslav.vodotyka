dcafd375034bdf8221d8ce7e65e91edddcb32738
Yaroslav Vodotyka <yaroslav.vodotyka@nure.ua>
2019-11-17 12:24:15 +0200

    UserDao interface created, custom Exception added

---
+++ src/main/java/db/UserDao.java
@@ -0,0 +1,17 @@
+package db;
+
+import java.util.Collection;
+
+import nure.cs.vodotyka.usermanagment.User;
+
+public interface UserDao {
+	User CreateUser(User user) throws DatabaseException;
+	
+	void UpdateUser(long userId) throws DatabaseException;
+	
+	void DeleteUser(long userId) throws DatabaseException;
+	
+	User GetUser(long userId) throws DatabaseException;
+	
+	Collection GetAll() throws DatabaseException;
+}
