569ea266cb8135c4f685d47c0d8b55f26fb18107
Yaroslav Vodotyka <yaroslav.vodotyka@nure.ua>
2019-11-17 13:34:14 +0200

    HsqlUserDao CreateUser method implemeted

--- src/main/java/db/HsqlDbUserDao.java
+++ src/main/java/db/HsqlDbUserDao.java
@@ -1,18 +1,56 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
 
 import nure.cs.vodotyka.usermanagment.User;
 
+import java.sql.PreparedStatement;
+import java.sql.CallableStatement;
+
 public class HsqlDbUserDao implements UserDao {
 
-	public HsqlDbUserDao() {
-		// TODO Auto-generated constructor stub
+	private static final String INSERT_QUERY = "INSERT INTO users (firstname, lastname, dateofbirth) VALUES(?,?,?)";
+	private ConnectionFactory connectionFactory;
+	
+	public HsqlDbUserDao(ConnectionFactory connectionFactoryParam){
+		connectionFactory = connectionFactoryParam;
 	}
 
 	public User CreateUser(User user) throws DatabaseException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Connection dbConnection = connectionFactory.CreateConnection();
+			PreparedStatement statement = 
+					dbConnection.prepareStatement(INSERT_QUERY);
+			statement.setString(1, user.getFirstName());
+			statement.setString(2, user.getLastName());
+			statement.setDate(3, new Date(user.getDateOfBirth().getTime()));
+			
+			int rowsAffected = statement.executeUpdate();
+			if(rowsAffected != 1){
+				throw new DatabaseException("Rows affected on query - " + rowsAffected);
+			}
+			CallableStatement callable = dbConnection.prepareCall("call IDENTITY()");
+			ResultSet keys = callable.executeQuery();
+			if(keys.next()){
+				user.setId(new Long(keys.getLong(1)));
+			}
+			
+			keys.close();
+			callable.close();
+			statement.close();
+			dbConnection.close();
+			
+			return user;
+		} catch (DatabaseException e){
+			throw e;
+		}
+		catch (SQLException e) {
+			throw new DatabaseException(e);
+		}
 	}
 
 	public void UpdateUser(long userId) throws DatabaseException {
