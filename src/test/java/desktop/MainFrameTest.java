1e9ca275d3373a5a6999f4978a39bd1cca11e391
Yaroslav Vodotyka <yaroslav.vodotyka@nure.ua>
2019-11-25 14:38:04 +0200

    feat(MainFrameTest) find component method

--- src/test/java/desktop/MainFrameTest.java
+++ src/test/java/desktop/MainFrameTest.java
@@ -1,7 +1,10 @@
 package desktop;
 
+import java.awt.Component;
+
 import junit.extensions.jfcunit.JFCTestCase;
 import junit.extensions.jfcunit.JFCTestHelper;
+import junit.extensions.jfcunit.finder.NamedComponentFinder;
 
 public class MainFrameTest extends JFCTestCase {
 
@@ -15,7 +18,16 @@ public class MainFrameTest extends JFCTestCase {
 	}
 
 	protected void tearDown() throws Exception {
+		mainFrame.setVisible(false);
+		getHelper().cleanUp(this);
 		super.tearDown();
+	}
+	
+	private Component Find(Class compClass, String name){
+		NamedComponentFinder finder = new NamedComponentFinder(compClass, name);
+		Component comp = finder.find(mainFrame, 0);
+		assertNotNull("Component was not found: " + name ,comp);
+		return comp;
 	}
 
 }
