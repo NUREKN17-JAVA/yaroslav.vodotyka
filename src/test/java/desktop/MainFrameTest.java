d9c0be38273d2d6b1605d8bf739d2115b2838d1c
Yaroslav Vodotyka <yaroslav.vodotyka@nure.ua>
2019-11-25 14:47:22 +0200

    feat(MainFrameTest) testBrowseControls method added

--- src/test/java/desktop/MainFrameTest.java
+++ src/test/java/desktop/MainFrameTest.java
@@ -2,6 +2,10 @@ package desktop;
 
 import java.awt.Component;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+
 import junit.extensions.jfcunit.JFCTestCase;
 import junit.extensions.jfcunit.JFCTestHelper;
 import junit.extensions.jfcunit.finder.NamedComponentFinder;
@@ -23,8 +27,19 @@ public class MainFrameTest extends JFCTestCase {
 		super.tearDown();
 	}
 	
+	//Test methods here
+	
+	public void testBrowseControls(){
+		Find(JPanel.class, "browsePanel");
+		Find(JTable.class, "userTable");
+		Find(JButton.class, "addButton");
+		Find(JButton.class, "deleteButton");
+		Find(JButton.class, "detailButton");
+	}
+	
 	private Component Find(Class compClass, String name){
 		NamedComponentFinder finder = new NamedComponentFinder(compClass, name);
+		finder.setWait(0);
 		Component comp = finder.find(mainFrame, 0);
 		assertNotNull("Component was not found: " + name ,comp);
 		return comp;
