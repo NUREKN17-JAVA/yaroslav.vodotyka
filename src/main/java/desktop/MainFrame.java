1772145a20e71983bb60e349857dd3b54e81d7e8
Yaroslav Vodotyka <yaroslav.vodotyka@nure.ua>
2019-11-25 14:31:45 +0200

    MainFrame anf MainFrameTest created

---
+++ src/main/java/desktop/MainFrame.java
@@ -0,0 +1,12 @@
+package desktop;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
