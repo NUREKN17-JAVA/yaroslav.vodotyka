33a502e163985e50e3a5fde5dc4610aca0f2186f
Yaroslav Vodotyka <yaroslav.vodotyka@nure.ua>
2019-12-03 10:22:03 +0200

    ref(MockServletTestCase) class renamed

--- src/test/java/web/MockservletTestCase.java
+++ src/test/java/web/MockservletTestCase.java
@@ -1,11 +1,32 @@
 package web;
 
+import java.util.Properties;
+
+import com.mockobjects.dynamic.Mock;
 import com.mockrunner.servlet.BasicServletTestCaseAdapter;
 
-public class MockservletTestCase extends BasicServletTestCaseAdapter {
+import db.DaoFactory;
+import db.MockDaoFactory;
+import db.MockUserDao;
 
+public class MockServletTestCase extends BasicServletTestCaseAdapter {
+
+	private Mock mockUserDao;
+	
 	protected void setUp() throws Exception {
+		Properties prop = new Properties();
+		prop.setProperty("dao.factory", MockDaoFactory.class.getName());
+		DaoFactory.init(prop);
+		setMockUserDao((MockDaoFatory)DaoFactory.GetInstance()).GetUserDao());
 		super.setUp();
+	}
+
+	public Mock getMockUserDao() {
+		return mockUserDao;
+	}
+
+	public void setMockUserDao(Mock mockUserDao) {
+		this.mockUserDao = mockUserDao;
 	}
 
 	protected void tearDown() throws Exception {
