69fbfd829079741273e56edc3abe74fa4df3c52b
Yaroslav Vodotyka <yaroslav.vodotyka@nure.ua>
2019-12-04 13:23:32 +0200

    feat(AddServletTest) test case created

---
+++ src/test/java/web/AddServletTest.java
@@ -0,0 +1,76 @@
+package web;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import nure.cs.vodotyka.usermanagment.User;
+
+public class AddServletTest extends MockServletTestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		createServlet(AddServlet.class);
+	}
+
+	public void testAdd(){
+		Date date = new Date();
+		User user = new User(new Long(1), "Yaroslav", "Vodotyka", date);
+		user.setId(null);
+		User createdUser = new User(new Long(1), "Yaroslav", "Vodotyka", date);
+		getMockUserDao().expectAndReturn("AddUser", user, createdUser);
+		
+		addRequestParameter("id", "1");
+		addRequestParameter("firstName", "Yaroslav");
+		addRequestParameter("lastName", "Vodotyka");
+		addRequestParameter("date", DateFormat.getDateInstance().format(date));
+		addRequestParameter("okButton");
+		doPost();
+	}
+	
+	public void testAddEmptyFirstName(){
+		Date date = new Date();
+		
+		addRequestParameter("id", "1");
+		addRequestParameter("lastName", "Vodotyka");
+		addRequestParameter("date", DateFormat.getDateInstance().format(date));
+		addRequestParameter("okButton");
+		doPost();
+		String errorMsg = (String)getWebMockObjectFactory().getMockRequest().getAttribute("error");
+		assertNotNull("Cant find a message in session scope", errorMsg);
+	}
+	
+	public void testAddEmptyLastName(){
+		Date date = new Date();
+		
+		addRequestParameter("id", "1");
+		addRequestParameter("firstName", "Yaroslav");
+		addRequestParameter("date", DateFormat.getDateInstance().format(date));
+		addRequestParameter("okButton");
+		doPost();
+		String errorMsg = (String)getWebMockObjectFactory().getMockRequest().getAttribute("error");
+		assertNotNull("Cant find a message in session scope", errorMsg);
+	}
+	
+	public void testAddEmptyDate(){
+		addRequestParameter("id", "1");
+		addRequestParameter("firstName", "Yaroslav");
+		addRequestParameter("lastName", "Vodotyka");
+		addRequestParameter("okButton");
+		doPost();
+		String errorMsg = (String)getWebMockObjectFactory().getMockRequest().getAttribute("error");
+		assertNotNull("Cant find a message in session scope", errorMsg);
+	}
+	
+	public void testAddDateValidate(){
+        Date date = new Date();
+		
+		addRequestParameter("id", "1");
+		addRequestParameter("firstName", "Yaroslav");
+		addRequestParameter("lastName", "Vodotyka");
+		addRequestParameter("date", "wrongFormatDate");
+		addRequestParameter("okButton");
+		doPost();
+		String errorMsg = (String)getWebMockObjectFactory().getMockRequest().getAttribute("error");
+		assertNotNull("Cant find a message in session scope", errorMsg);
+	}
+}
