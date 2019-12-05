97f2061b6912a2e2926a7cf815f842d48ecbafe7
Yaroslav Vodotyka <yaroslav.vodotyka@nure.ua>
2019-12-05 11:12:15 +0200

    feat(RequestServer) first implementation

---
+++ src/main/java/agent/RequestServer.java
@@ -0,0 +1,13 @@
+package agent;
+
+import jade.core.behaviours.CyclicBehaviour;
+
+public class RequestServer extends CyclicBehaviour {
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
